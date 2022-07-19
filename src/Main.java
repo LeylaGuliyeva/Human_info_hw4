1f29162b2c1cffa0c0616331c0f9cfd17a304f26
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-07-19 20:04:18 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,26 @@
 
 public class Main {
     public static void main(String[] args) {
-        Human father1=new Human("Ali","Aliyev",1970,"Zehra","Rustam");
-        Pet p1;
-        Human mother1=new Human("Jamila","R");
-        Human child1;
+         Pet hamster=new Pet();
+        Pet carrot=new Pet("Carrot","Chuky");
+        Pet cat=new Pet("Cat","Kandela",2,99,new String[]{"Eating","Sleeping","Creating a mess"});
+        Human Samin=new Human("Samin","Alakbarov",1982);
+        Human Lala=new Human("Lala","Alakbarova",2003) ;
+        Human Eren=new Human("Eren","Alakbarov",2029,Lala,Samin);
+        String sl[][]={{"Monday","Tuesday","Wednesday","Thursday","Friday"},{"Playing piano","Doing math","Yoga","Reading book","Watching AOT"}};
+        byte iq=100;
+        Human Mikasa=new Human("Mikasa","Alakbarova",2031,iq,cat,Lala,Samin,sl);
+        Human nobody=new Human();
+        Mikasa.greetPet();
+        Mikasa.describePet();
+        System.out.println(cat);
+        System.out.println(carrot);
+        System.out.println(hamster);
+        System.out.println(Samin);
+        System.out.println(Lala);
+        System.out.println(Eren);
+        System.out.println(Mikasa);
+        System.out.println(nobody);
     }
 
 }
